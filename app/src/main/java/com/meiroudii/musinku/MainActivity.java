90ef43da7a1020a4f538d6890267e36aec1519fd
Meiroudii <joelllena123@gmail.com>
2024-04-05 19:54:43 +0800

    test: read to storage

--- app/src/main/java/com/meiroudii/musinku/MainActivity.java
+++ app/src/main/java/com/meiroudii/musinku/MainActivity.java
@@ -1,14 +1,31 @@
 package com.meiroudii.musinku;
 
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Imported when activity_main.xml
+    RecyclerView recyclerView;
+    TextView noMusicTextView;
+    ArrayList<AudioModel> songsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,60 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // TextVariables
+        recyclerView = findViewById(R.id.recycler_view);
+        noMusicTextView = findViewById(R.id.no_songs_text);
+
+        if(checkPermission() == false) {
+            requestPermission();
+            return;
+        }
+
+        // Attributes that must retrieve to the songs
+        String[] projection = {
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DURATION
+        };
+
+       String selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
+
+        // Quering the database to view the list of songs
+        // all the song data will store into the Cursor
+        Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, selection, null, null);
+        while(cursor.moveToNext()) {
+            AudioModel songData = new AudioModel(cursor.getString(1), cursor.getString(0), cursor.getString(2));
+            if(new File(songData.getPath()).exists())
+                songsList.add(songData);
+        }
+
+        if(songsList.size()==0) {
+            noMusicTextView.setVisibility(View.VISIBLE);
+        }else {
+            // Recycler view
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter();
+        }
+    }
+
+    // Creating a method in retrieving the songs
+    boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+    // If you remove the void, the user can't deny your permission
+    void boolean requestPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            Toast.makeText(MainActivity.this, "I CAN'T FIND ANY SONGS IF YOU DENIED ME!", Toast.LENGTH_SHORT).show();
+        }else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 123);
+        }
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 123);
     }
 }
